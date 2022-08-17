da7fad139310a693114e12173d8823ce11198ac6
Apurv gupta <56751664+apurvgupta-ops@users.noreply.github.com>
2022-08-17 16:36:34 +0530

    Array

---
+++ 450 QUESTIONS/Array/ReverseArray.cpp
@@ -0,0 +1,72 @@
+// #include <iostream>
+// using namespace std;
+
+// void reverseArray(int arr[], int start, int end)
+// {
+
+//     while (start < end)
+//     {
+//         int temp = arr[start];
+//         arr[start] = arr[end];
+//         arr[end] = temp;
+//         start++;
+//         end--;
+//     }
+// }
+
+// void printArray(int arr[], int size)
+// {
+//     for (int i = 0; i < size; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
+// }
+
+// int main()
+// {
+
+//     int arr[] = {1, 2, 3, 4, 5};
+//     int size = sizeof(arr) / sizeof(arr[0]);
+//     printArray(arr, size);
+//     reverseArray(arr, 0, size - 1);
+//     cout << "Reversed Array is" << endl;
+//     printArray(arr, size);
+// }
+
+// _________________________________________________________
+// Recursive approch
+#include <iostream>
+using namespace std;
+
+void reverseArray(int arr[], int start, int end)
+{
+    // edge case
+    if (start >= end)
+    {
+        return;
+    }
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+
+    reverseArray(arr, start + 1, end - 1);
+}
+
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printArray(arr, n);
+    reverseArray(arr, 0, n - 1);
+    printArray(arr, n);
+}
