a4c539a354970f5700fefd0f78c58b5a97f10152
Apurv gupta <56751664+apurvgupta-ops@users.noreply.github.com>
2022-09-10 00:40:44 +0530

    String

--- 450 QUESTIONS/Strings/SortAStringAccordingToTheFrequencyOfCharacters.cpp
+++ 450 QUESTIONS/Strings/SortAStringAccordingToTheFrequencyOfCharacters.cpp
@@ -3,6 +3,41 @@
 // Time Complexity: O(n2)
 // Auxiliary Space: O(n)
 
-#include<iostream>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
+int countFreq(string str, char ch)
+{
+    int n = str.length();
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (str[i] == ch)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+void func(string str)
+{
+    int n = str.length();
+    vector<pair<int, char>> v;
+    for (int i = 0; i < n; i++)
+    {
+        v.push_back(make_pair(countFreq(str, str[i]), str[i]));
+    }
+    sort(v.begin(), v.end());
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i].second << " ";
+    }
+}
+
+int main()
+{
+    string str = "dsdfsadffds";
+    func(str);
+}
