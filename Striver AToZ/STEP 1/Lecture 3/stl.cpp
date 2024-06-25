d2c75b893e1f005ebac5a13d048862c3b9a2c0d3
Apurv gupta <56751664+apurvgupta-ops@users.noreply.github.com>
2024-06-25 23:32:57 +0530

    stl list, stack

--- Striver AToZ/STEP 1/Lecture 3/stl.cpp
+++ Striver AToZ/STEP 1/Lecture 3/stl.cpp
@@ -22,6 +22,7 @@ void explainVector()
     v.push_back(1);
     v.emplace_back(2); // pushback and emplaceback is same, but emplaceback takes less time to add the data;
 
+    // ? LOOP OVER THE VECTOR
     for (vector<int>::iterator it = v.begin(); it != v.end(); it++)
     {
         cout << *it << " ";
@@ -37,6 +38,54 @@ void explainVector()
     }
 
     // ? DELETE THE ELEMENTS FROM THE VECTOR;
+    v.erase(v.begin() + 1);
+    v.erase(v.begin() + 1, v.begin() + 4);
+
+    // ? INSERT
+    v.insert(v.begin(), 600);
+
+    // ? SIZE
+    v.size();
+
+    // ? REMOVE THE LAST ELEMENT
+    v.pop_back();
+
+    // ? SWAP
+    vector<int> v2;
+    v.swap(v2);
+
+    // ?CLEAR WHOLE VECTOR
+    v.clear();
+}
+
+void explainList()
+{
+    // * List provide the front operation means it provide the function to add the data in the front of the list
+    list<int> ls;
+    ls.push_front(1);
+
+    // rest of the functions are same as vector;
+}
+
+void explainDeque()
+{
+    // ? this is combined functions of vector and list
+}
+
+void explainStack()
+{
+    stack<int> st;
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.push(5);
+
+    st.top();
+
+    st.pop();
+
+    // remaining all are same as vector;
 }
 int main()
 {
